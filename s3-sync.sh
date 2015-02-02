6e121a9fe0bc15c523d6f97285eee1c18e40c1c2
Will Chester <william.chester@gmail.com>
2015-02-02 17:29:30 -0500

    updated s3-sync to use aws cli instead of s3cmd

--- s3-sync.sh
+++ s3-sync.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 # Show example output when no arguments provided
-usage() { echo "Usage: $0 [-c <path to configuration file>] [-p <path to s3cmd executable> ] [-o <override optional s3cmd defaults> ] [-d <perform a dry-run> ] [-l <path of optional logfile> ] <path to file/folders to be backed up> <name of s3://example-bucket>" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-p <path to aws executable> ] [-o <aws options> ] [-d <perform a dry-run> ] [-l <path of optional logfile> ] <path to file/folders to be backed up> <name of s3://example-bucket>" 1>&2; exit 1; }
 
-# Set default s3cfg location to s3cmd default
-default_s3cfg="$HOME/.s3cfg"
-
-# Set default s3cmd executable path
-default_s3cmd_path=$(which s3cmd)
-
-# Set default s3cmd options
-default_s3cmd_options="--acl-private -s --server-side-encryption --cache-file $HOME/.cache/s3cmd/cache-file"
+# Set default executable path
+default_cmd_path=$(which aws) 
 
 # Add CURL email support
 # Set to false if not sending error messages
@@ -22,36 +16,22 @@ send_curl_mail=false
 # mail_api_key=
 
 # Parse arguments
-while getopts ":c:p:o:dl:" opt; do
+while getopts ":p:o:dl:" opt; do
     case $opt in
-    c) # Manually specified configuration file
-        # Set s3cfg var equal to passed in argument
-        s3cfg=$OPTARG
+    p) # Manually specified path
+        cmd_path=$OPTARG
 
-        # Throw a fatal error if the specified file does not exist
-        if [[ ! -e $s3cfg ]]; then 
-            echo "FATAL: Specified file $s3cfg does not exist! Check the path and try again or run s3cmd --configure to create a .s3cfg file." >&2; exit 1;
+        # Throw a fatal error if the specified path to does not exist
+        if [[ ! -x $cmd_path ]]; then 
+            echo "FATAL: Specified executable $cmd_path does not appear to exist! Check the path and try again." >&2; exit 1;
         fi
         ;;
-    p) # Manually specified s3cmd path
-        s3cmd_path=$OPTARG
-
-        # Throw a fatal error if the specified path to s3cmd does not exist
-        if [[ ! -x $s3cmd_path ]]; then 
-            echo "FATAL: Specified executable $OPTARG does not appear to exist! Check the path and try again." >&2; exit 1;
-        fi
-        ;;
-    o) # String of all s3cmd options
-        s3cmd_options="${OPTARG}"
-
-        # Override defaults if options exist
-        if [[ -z "${s3cmd_options}" ]]; then
-            echo "FATAL: No options found! Check the options and try again." >&2; exit 1;
-        fi
+    o) # String of all options
+        options="${OPTARG}"
         ;;
     d) # Perform a dry-run
         # Set dry run variable
-        s3cmd_dry_run="--dry-run"
+        dry_run="--dryrun"
         ;;
     l) # Add logging
         logfile=$OPTARG
@@ -99,30 +79,21 @@ if [[ $logging_on ]]; then
     echo ""
 fi
 
-# Use the default configuration if no config file specified
-if [[ -z "$s3cfg" && -e $default_s3cfg ]]; then
-    echo "INFO: Using default s3cmd configuration in $HOME/.s3cfg"
-    s3cfg="$default_s3cfg"
+# Use the default path if no path specified
+if [[ -z "$cmd_path" && -x $default_cmd_path ]]; then
+    echo "INFO: Using default executable $default_cmd_path"
+    cmd_path=$default_cmd_path
 fi
 
-# Add flag for s3cfg
-s3cfg="-c $s3cfg"
-
-# Use the default s3cmd path if no path specified
-if [[ -z "$s3cmd_path" && -x $default_s3cmd_path ]]; then
-    echo "INFO: Using default s3cmd executable $default_s3cmd_path"
-    s3cmd_path=$default_s3cmd_path
+# Throw error if no path set by this point
+if [[ -z "$cmd_path" ]]; then
+    echo "FATAL: No aws executable found. Check that awscli is installed or specify a path using the -p flag." >&2; exit 1;
 fi
 
-# Throw error if no s3cmd path set by this point
-if [[ -z "$s3cmd_path" ]]; then
-    echo "FATAL: No s3cmd executable found. Check that s3cmd is installed by running which s3cmd or specify a path using the -p flag." >&2; exit 1;
-fi
-
-# Use the default s3cmd_options if none specified
-if [[ -z "$s3cmd_options" ]]; then
-    echo "INFO: Using default s3cmd options $default_s3cmd_options"
-    s3cmd_options=$default_s3cmd_options
+# Use the default options if none specified
+if [[ -z "$options" ]]; then
+    echo "INFO: Using default options $default_options"
+    options=$default_options
 fi
 
 # Check that sync target and path have been specified
@@ -131,8 +102,8 @@ if [[ $# != 2 ]]; then
     usage
 fi
 
-# Run s3cmd command with provided variables
-$s3cmd_path sync $s3cfg $s3cmd_options $s3cmd_dry_run "$1" "$2"
+# Run command with provided variables
+$cmd_path s3 sync "$1" "$2" $options $dry_run 
 
 # Add to log if enabled
 if [[ $logging_on ]]; then
